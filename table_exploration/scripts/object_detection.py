185948390a7c1b2309759583c89e41f9f0a6059c
Katerina M. Oikonomou <k.m.oikonomou@gmail.com>
2022-06-01 19:43:02 +0300

    TODO: fix distance estimation :wrench:

--- table_exploration/scripts/object_detection.py
+++ table_exploration/scripts/object_detection.py
@@ -22,11 +22,12 @@ class CascadeBoxDetector(object):
         self.dist_pub = rospy.Publisher('/kuka/box/distance', Distance, queue_size=10)
         self.box_pub = rospy.Publisher(self.detector_topic, Image, queue_size=1)
         self.cascade_pub = rospy.Publisher('/kuka/cascade/detection', Image, queue_size=1)
-        self.rec_image = Image()
-        # self.image_inf = [0,self.rec_image]
         self.box_color = (0,255,0)
-        self.real_width = 10
-        self.known_distance = 78.8
+        self.real_width = 0.10
+        self.known_distance = 7.88
+        self.rec_image = Image()
+        self.image_rect = [0,self.rec_image]
+        self.crop_img = Image()
 
     def on_image(self,msg):
         self.last_msg = msg
@@ -35,29 +36,29 @@ class CascadeBoxDetector(object):
     def draw_rectangles(self, im, detect):
         for (x,y,h,w) in detect:
             rect = cv2.rectangle(im, (x,y),(x+w,y+h), self.box_color, 3)
-            rec_image = cv_bridge.cv2_to_imgmsg(rect)
-            image_inf=[w,rec_image]
-        return image_inf
+            self.image_rect = cv_bridge.cv2_to_imgmsg(rect)
+            self.image_rect=[w,self.image_rect]
+        return self.image_rect
 
     def crop_image(self, img, detect):
         for (x,y,h,w) in detect:
-            crop_img = img[y:y+h,x:x+w]
-            # crop_img = cv_bridge.cv2_to_imgmsg(crop_img)
-        return crop_img
+            self.crop_img = img[y:y+h,x:x+w]
+        return self.crop_img
 
     def mask(self,image):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
         min_red = np.array([0, 0, 50])
         max_red = np.array([50, 50, 255])
-        # min_red = np.array([155,25,0])
-        # max_red = np.array([179,255,255])
+
 
         mask = cv2.inRange(hsv, min_red, max_red) 
         output = cv2.bitwise_and(image, image, mask=mask)
         cnts,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # cnts = imutils.grab_contours(cnts)
-        # c = max(cnts, key = cv2.contourArea)
+        if cv2.countNonZero(mask) > 0:
+            print('Red is present!')
+        else:
+            print('Red is not present!')
         return cnts
 
     def focal_length(self, measured_distance, real_width, width_in_rf_image):
@@ -76,14 +77,14 @@ class CascadeBoxDetector(object):
 
     def spin(self):
         rate = rospy.Rate(self.rate)
-        global object_width
+        # global object_width
         # object_width = 0.1
-        rate = rospy.Rate(self.rate)
+
         ref_image = cv2.imread("red_gazebo.png")
         ref_image_width = self.mask(ref_image)
-        object_width = ref_image_width[1][0]
-        focal_length = self.focal_length(self.known_distance,self.real_width, object_width)
+        object_width = self.width_in_rfimg(ref_image_width)
 
+        focal_length = self.focal_length(self.known_distance,self.real_width, object_width)
         while not rospy.is_shutdown():
             rate.sleep()
 
@@ -92,22 +93,23 @@ class CascadeBoxDetector(object):
                 continue
             try:
                 image = cv_bridge.imgmsg_to_cv2(self.last_msg, "bgr8")
-                # self.last_msg = None
                 detections = self.cascade.detectMultiScale(image, 1.2, 5)
                 crop_img = self.crop_image( image, detections)
                 rec_image = self.draw_rectangles( image , detections)
-
                 # cv2.imshow('Image', crop_img)
                 # cv2.waitKey(0) 
                 marker = self.mask(crop_img)
-                draw_im = cv2.drawContours(crop_img, marker, -1, (0, 255, 0), 2)
+                draw_im = cv2.drawContours(crop_img, marker, -1, self.box_color, 2)
                 red_obj = cv_bridge.cv2_to_imgmsg(draw_im)
                 self.box_pub.publish(red_obj)
                 object_width = self.width_in_rfimg(marker)
-                distance = self.distance(focal_length,self.real_width, object_width)
-                print(distance)
+                if object_width >= 0.10:
+                    distance_est = self.distance(focal_length, self.real_width, object_width)
+                else:
+                    print("Distance not found")
+                print("Distance",distance_est)
                 msg = Distance()
-                msg.data = distance
+                msg.data = distance_est
                 msg.stamp = self.stamp
                 self.dist_pub.publish(msg)
                 self.cascade_pub.publish(rec_image[1])
