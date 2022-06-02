48e17b8368e7582567b21716202f2a9c38238bb7
Katerina M. Oikonomou <k.m.oikonomou@gmail.com>
2022-06-02 17:12:21 +0300

    Calculate distance from collisions

--- table_exploration/scripts/collision_distance.py
+++ table_exploration/scripts/collision_distance.py
@@ -3,23 +3,24 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
 
-pub = None
 
 def callback(msg):
     dist = min(msg.ranges[288:431])
-    distance(dist)
-def distance(dist):
-    pub_msg = Float64()
-    pub_msg.data = dist
-    pub.publish(pub_msg)
+    test= Float64()
+    test.data = dist
+    # print(dist)
+    pub.publish(test)
 
 def main():
     global pub
     rospy.init_node('collision')
-    pub = rospy.Publisher('/collision', Float64, queue_size=1)
+    pub = rospy.Publisher('/kuka/collision', Float64, queue_size=1)
     sub = rospy.Subscriber('/kuka/laser/scan', LaserScan, callback)
     rospy.spin()
 
 
 if __name__=='__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
