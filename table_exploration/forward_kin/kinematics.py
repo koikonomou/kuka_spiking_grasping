ae5490734a0f58bf5002477178e3cdcda0931df6
Katerina Oikonomou <aioikono@pme.duth.gr>
2022-06-18 19:34:26 +0300

    Updated

--- table_exploration/forward_kin/kinematics.py
+++ table_exploration/forward_kin/kinematics.py
@@ -4,8 +4,9 @@ import numpy as np
 chain = kp.build_chain_from_urdf(open("kuka_model.urdf").read())
 print(chain)
 print(chain.get_joint_parameter_names())
-th = [0.0, -np.pi / 4.0, 0.0, np.pi / 7.0, 0.0, np.pi / 4.0]
+th = [0.0, np.pi /180, 190 * np.pi / 180, 0.0, 35 * np.pi / 180, 0.0]
 ret = chain.forward_kinematics(th)
+
 print(ret)
 viz = kp.Visualizer()
 viz.add_robot(ret, chain.visuals_map(), axes=True)
